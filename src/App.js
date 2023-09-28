e78d0f3adb2c9b4d1c414bc65f4f6adea951bcaf
noman-nahil <abdullahnoman1997@gmail.com>
2023-09-28 19:26:12 +0600

    Create the Navbar component in React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Navbar from "./components/navbar";
+
 function App() {
-  return <h1 className="text-3xl font-bold ">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
