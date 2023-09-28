344f239601dba52befdda9f86aede24205f054cd
noman-nahil <abdullahnoman1997@gmail.com>
2023-09-29 03:10:13 +0600

    Implement grocery product details modal on card click

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -5,6 +5,8 @@ import axios from "axios";
 const Body = () => {
   const [products, setProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [item, setItem] = useState([]);
   useEffect(() => {
     // Make a GET request to your server to fetch grocery products
     axios
@@ -24,6 +26,16 @@ const Body = () => {
   const filteredProducts = products.filter((product) =>
     product.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+    setItem([]);
+  };
+  const modalHandle = (product) => {
+    const selectedItem = product;
+
+    setIsOpen(!isOpen);
+    setItem(selectedItem);
+  };
 
   return (
     <div className="mx-4 my-4md:mx-16 md:my-10">
@@ -34,7 +46,7 @@ const Body = () => {
         placeholder="Search..."
         onChange={searchHandle}
       />
-      <div className="grid  grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
         {filteredProducts.map((product) => (
           <div
             className="max-w-sm  rounded overflow-hidden shadow-lg"
@@ -52,6 +64,7 @@ const Body = () => {
                 <button
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm ml-auto  p-2 "
+                  onClick={() => modalHandle(product)}
                 >
                   See more
                 </button>
@@ -60,6 +73,48 @@ const Body = () => {
           </div>
         ))}
       </div>
+      {isOpen && (
+        <div className="fixed inset-1/4   w-2/3  lg:w-2/4  h-full  ">
+          <div className="modal-content bg-white p-6 rounded-lg shadow-lg border border-2 ">
+            {/* <h2 className="text-xl font-semibold mb-4">{item.name}</h2> */}
+            <div className="flex">
+              <button
+                onClick={toggleModal}
+                className=" bg-white  text-black font-bold py-2 px-4 rounded ml-auto"
+              >
+                X
+              </button>
+            </div>
+            <img
+              className="h-44 md:h-48 lg:h-52 xl:h-56  mx-auto"
+              src={item.link}
+            />
+            <div className="px-6 pt-4 pb-2  ">
+              <div className="flex">
+                <p className="font-bold w-1/2">{item.name}</p>
+                <p className="text-sm  hover:bg-blue-800  font-medium rounded-lg text-sm ml-auto  p-2 ">
+                  Price : {item.price} $
+                </p>
+              </div>
+            </div>
+            <span className="">{item.description}</span>
+            <div className="flex">
+              <button
+                onClick={() => alert(item.name + " added in cart")}
+                className="mt-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-auto"
+              >
+                Add to cart
+              </button>
+              <button
+                onClick={toggleModal}
+                className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+              >
+                Close Modal
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
