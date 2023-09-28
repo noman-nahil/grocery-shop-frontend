a8fa2bce4ac971d776a26cbd2ff3981023d99220
noman-nahil <abdullahnoman1997@gmail.com>
2023-09-29 01:33:43 +0600

    Fix navbar issues and add grocery product search functionality

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const Body = () => {
   const [products, setProducts] = useState([]);
-
+  const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
     // Make a GET request to your server to fetch grocery products
     axios
@@ -17,11 +17,25 @@ const Body = () => {
         console.error("Error fetching data:", error);
       });
   }, []);
+  const searchHandle = (e) => {
+    console.log(e.target.value);
+    setSearchTerm(e.target.value);
+  };
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
-    <div className="mx-16 my-10">
+    <div className="mx-4 my-4md:mx-16 md:my-10">
+      <input
+        type="text"
+        id="search-navbar"
+        className="block  lg:w-1/3 p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg mb-5 mx-auto mt-4"
+        placeholder="Search..."
+        onChange={searchHandle}
+      />
       <div className="grid  grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <div
             className="max-w-sm  rounded overflow-hidden shadow-lg"
             key={product.id}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,12 +9,13 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" className="flex items-center">
-          <img className="h-10" src={logo} />
+        <a className="flex items-center">
+          <img className="h-10" src={logo} alt="logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
             Grocery Shop
           </span>
         </a>
+
         <button
           data-collapse-toggle="navbar-default"
           type="button"
@@ -48,14 +49,9 @@ const Navbar = () => {
           }
           id="navbar-default"
         >
-          <ul className="mr-6 font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="mr-6 font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0">
             <li>
-              <button className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                Admin Dashboard
-              </button>
-            </li>
-            <li>
-              <button className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+              <button className="block py-2 pl-3 pr-4 text-gray-50 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
                 Cart
               </button>
             </li>
